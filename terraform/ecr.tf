7d221fd03c8fdb95d2ac69d12a8444897578aa85
daoqi <45522065+daochidq@users.noreply.github.com>
2025-05-20 14:34:08 -0400

    remove unnecessary github action and update ui deployment

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -6,3 +6,12 @@ resource "aws_ecr_repository" "librechat" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository" "prowler_ui" {
+  name                 = "prowler-ui-image"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
