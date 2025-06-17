5cc9b2341d4391019192510a096f01ab78479bde
daoqi <45522065+daochidq@users.noreply.github.com>
2025-06-17 11:45:29 -0400

    update prowler

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,11 +1,14 @@
-# resource "helm_release" "test_prowler_release" {
-#   name      = "prowler-api"
-#   namespace = "prowler"
-#   create_namespace = true
+resource "helm_release" "prowler_release" {
+  name      = "prowler"
+  namespace = "prowler"
+  create_namespace = true
   
-#   repository          = "oci://897729109735.dkr.ecr.us-east-1.amazonaws.com"
-#   chart               = "prowler-api"
-#   version             = "0.1.0"
+  repository          = "oci://897729109735.dkr.ecr.us-east-1.amazonaws.com"
+  chart               = "prowler-api"
+  version             = "0.1.0"
+  values = [
+    "${file("prowler.yaml")}"
+  ] 
  
-# }
+}
 
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,16 +6,20 @@ data "aws_ecr_authorization_token" "token" {
   registry_id = "897729109735"
 }
 
+data "aws_eks_cluster" "s-api" {
+  name = "ascending-s-api-cluster"
+}
+
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    host                   = data.aws_eks_cluster.s-api.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.s-api.certificate_authority[0].data)
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", "ascending-s-api-cluster"]
     }
   }
 
